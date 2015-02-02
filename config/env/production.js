1afbed5991ee07c3e94e9b7ae7cbdcd60bca5fc5
Shiftane <shiftane@gmail.com>
2015-02-02 09:51:58 +0100

    Update production DB config

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://localhost/mean-prod',
+  db: 'mongodb://heroku_app33656042:xxxx@ds039431.mongolab.com:39431/heroku_app33656042',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
