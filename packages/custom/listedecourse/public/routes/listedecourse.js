8b2d60fa1916d4560561565df3b8cd134415e922
Shiftane <shiftane@gmail.com>
2015-02-25 09:20:57 +0100

    change url

--- packages/custom/listedecourse/public/routes/listedecourse.js
+++ packages/custom/listedecourse/public/routes/listedecourse.js
@@ -1,7 +1,9 @@
 'use strict';
 
-angular.module('mean.listedecourse').config(['$stateProvider',
-  function($stateProvider) {
+angular.module('mean.listedecourse').config(['$stateProvider','$locationProvider',
+  function($stateProvider, $location) {
+  	$location.html5Mode(html5pushstate);
+    $location.hashPrefix('');
 
     $stateProvider.state('Homepage', {
       url: '/listedecourse/home',
